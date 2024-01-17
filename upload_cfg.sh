4695b7ef8c9ddd4139fcc3f482e5a74b3e77857d
Stanislav Pirx <stanislav.pirx@gmail.com>
2024-01-17 18:37:18 +0300

    update input shaper config
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- upload_cfg.sh
+++ upload_cfg.sh
@@ -1,3 +1,4 @@
 scp tmc.cfg flying_bear:/home/orangepi/printer_data/config/tmc.cfg
 scp printer.cfg flying_bear:/home/orangepi/printer_data/config/printer.cfg
+scp ./boot/orangepiEnv.txt root@flying_bear:/boot/orangepiEnv.txt 
 
